9dc8dfe87893514efcf313ea6ff42e7ad0edf9ed
helium96 <masters2020@gmail.com>
2016-04-30 01:22:52 -0500

    First

---
+++ README.md
@@ -0,0 +1,15 @@
+#System Monitor
+
+Program to give temp of system and system components
+
+
+
+####
+'''bash
+	sudo apt-get install lm-sensors
+	sudo pip install pysensors
+'''
+
+'''python
+	pip install -U -r requirements.txt 
+'''
---
+++ monitor.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import sensors
+from twython import Twython
+
+#COLORS
+HEADER = '\033[95m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = "\033[1m"
+
+APP_KEY = 'PVYhYZxe4vgJ7fRlQdjNxLdaB'  # Customer Key here
+APP_SECRET = 'HIQRXDQ5jPGoDGXhtlNzHyGzbPqrCAdYdAjhVj48YnkzYJapP9'  # Customer secret here
+OAUTH_TOKEN = '382404070-T9EnACzBokbgV70AIlgusAmMP4dO1qt1hdthQlam'  # Access Token here
+OAUTH_TOKEN_SECRET = 'vpAuDjHEX9IeiavT3BEJ5xuxoFQppmuMGZLYtiCn6jQ96'  # Access Token Secret here
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+def getTemp():
+    sensors.init()
+    avg = 0
+    try:
+        for chip in sensors.iter_detected_chips():
+            print HEADER + '%s at %s' % (chip, chip.adapter_name) + ENDC
+            for feature in chip:
+                _system = feature.label
+                _system_temp = feature.get_value()
+                print BLUE + '\tSystem: %s ' % (_system) + ENDC
+                print WARNING + '\tTemp: %.2f°C ' % (_system_temp) + ENDC
+                if _system_temp > 60:
+                    print FAIL + '\tSystem Overheating' + ENDC
+                else:
+                    print GREEN + BOLD + '\tSystem OK' + ENDC
+                print '\n'
+    finally:
+        sensors.cleanup()
+
+
+if __name__ == "__main__":
+    getTemp()
+
+
+
+
+
+
